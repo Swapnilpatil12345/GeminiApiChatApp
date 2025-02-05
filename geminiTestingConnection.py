e1898c30c6da7e74626c5a8d909eb78513ef49d1
Swapnil <swapnil.22210061@viit.ac.in>
2025-02-05 14:56:35 +0530

    Update geminiTestingConnection.py

--- geminiTestingConnection.py
+++ geminiTestingConnection.py
@@ -3,7 +3,7 @@ import streamlit as st
 import getpass
 
 
-genai.configure(api_key="AIzaSyAqCMVk7G_EbHiamCzFel1ir2Ryo2IcXYA")
+api_key = st.secrets["GEMINI_API"]
 
 # Initialize the Gemini model
 model = genai.GenerativeModel("gemini-pro")
